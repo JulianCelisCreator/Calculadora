01b89d827bfa4263cea7b592bc3cef34edc5977b
Julian <celisjuliandavid@gmail.com>
2024-09-13 18:58:03 -0500

    New version

--- calculator_class.py
+++ calculator_class.py
@@ -11,148 +11,147 @@
 # Google Doc Python: Documentación python para cada método
 # Doc String
 from abc import ABC, abstractmethod
-import numpy as np
+import numpy as np  # Corregido: import numpy as np
 from datetime import datetime
-class AbstractCalculator(ABC): 
-    """"
+
+
+class AbstractCalculator(ABC):
+    """ "
     THis class represents the behavior of an abstract calculator.
     """
-    def suma (self, a:float, b:float)->float:
-        """ This methos sums two numerical values. #Resumen
-        This methot takes two arguments, expected as numbers,
-        and calculate and return the sum of those ones.     
 
-        Args: 
+    def suma(self, a: float, b: float) -> float:
+        """This methos sums two numerical values. #Resumen
+        This methot takes two arguments, expected as numbers,
+        and calculate and return the sum of those ones.
+
+        Args:
             a(float): First number of the sum.
             b(float): Second number of the sum
-            
+
         Returns:
             A float value with the result of the sum
         """
-        return a+ b 
-    
-    def rest(self,a: float, b: float) -> float:
+        return a + b
+
+    def rest(self, a: float, b: float) -> float:
         """This method applies substraction between two numbers
-        
+
         This method takes two numbers received as arguments and calculate the substraction in the given order.
-        
-        Args: 
+
+        Args:
             a(float): first numert of the rest
             b(float): Second number of the rest
-            
+
         returns:
             A float with the result of the substraction
-                
+
         """
         return a - b
-    
-    def multiplication(self, a:float, b:float) -> float:
+
+    def multiplication(self, a: float, b: float) -> float:
         """This method applies multiplication between two numbers
-        
+
         THis method takes two numbers received as arguments and calculate the substraction in the given order
-        
-        Args: 
+
+        Args:
             a(float): first number of the multiplication
             b(float): Second number of the multiplication
-            
+
         returns:
             A float with the result of the multiplication
-                
+
         """
         return a * b
-    
-    def division(self, a:float, b:float) -> float:
+
+    def division(self, a: float, b: float) -> float:
         """This method calculates the division between two numbers
-        
-        THis method takes two numbers received as arguments and calculate the division using 
+
+        THis method takes two numbers received as arguments and calculate the division using
         the native arithmetic operation, but without valition of zero-division
-        
-        Args: 
+
+        Args:
             a(float): numerator of the division
             b(float): divisor number of the division
-            
+
         returns:
             A float with the result of the division
-                
+
         """
         return a / b
-    # metodo abstract tiene la obligación de aparecer en las clases hijas  
+
+    # metodo abstract tiene la obligación de aparecer en las clases hijas
     @abstractmethod
     # Método abstracto
-    def power(base:int,exponent:int) -> int:
+    def power(self, base: int, exponent: int) -> int:
         """This is an abstract method for calculater power
-        
-        Args: 
+
+        Args:
             base(int): base of the power
             b(float): exponent of the power
-            
+
         returns:
             A int with the result of the power
-                
+
         """
-        
-        return base ** exponent
-    
+
+        return base**exponent
+
+
 # ================== Concrete Version of a calculator ============================ #
+
 
 class SimpleCalculator(AbstractCalculator):
     """This class represents the behavior of a simple concrete calculator
     where the methods from abstract class are ihnerated, some improvement
     and new functionalities ared added.
     """
-    
-    def __init__ (self):
+
+    def __init__(self):
         self.memory = 0
-        
-    def division(a:float,b:float) ->float:
-        
+
+    def division(a: float, b: float) -> float:
         """This method calculates the division between two numbers
-        
-        This method takes two numbers received as arguments and calculate the division using 
+
+        This method takes two numbers received as arguments and calculate the division using
         the native arithmetic operation, but including valitaion of zero-division
-        
-        Args: 
+
+        Args:
             a(float): numerator of the division
             b(float): divisor number of the division
-            
+
         returns:
             A float with the result of the division
-                
+
         """
         try:
-            result = a/b;
-        except Exception as e: 
+            result = a / b
+        except Exception as e:
             print(f"ERROR.{e}")
             # Abre log y agrega los errores al log, encoding son los simbolos del teclado
-            with open('log.txt', 'a', encoding = 'utf-8') as f:
-                f.write(f"{datetime.now()} --- ERROR. {e}. ") 
-            result = np.nan #Not a number
+            with open("log.txt", "a", encoding="utf-8") as f:
+                f.write(f"{datetime.now()} --- ERROR. {e}. ")
+            result = np.nan  # Not a number
         return result
-    
-    def power(base:int,exponent:int) -> int:
+
+    def power(base: int, exponent: int) -> int:
         """This is an abstract method for power calculation
-        
+
         In this method the power is calculated using a loop
         to apply multiplication in a iterative way
-        Args: 
+        Args:
             base(int): base of the power
             b(float): exponent of the power
-            
+
         returns:
             A integer with the result of the power
-                
+
         """
         if exponent == 0:
             result = 1
-        else: 
+        else:
             temp = 1
-            for i in range(exponent+1):
+            for i in range(exponent + 1):
                 temp *= base
             result = temp
         return result
-    
-
-        
-    
-        
-        
--- cli.py
+++ cli.py
@@ -5,8 +5,23 @@
     This file is parte of PyCalculator - UD
     
     PyCalculator 
-    
+    #       Tarea 1
+    #   Configurar entorno virtua de python
+    #   Activar entorno virtual
+
+    #       Tarea 2
+    #   pypi
+    #       Tarea 3
+    #   PEP (Sugerencias de adición o cambios para Python)
+    #       Tarea 4
+    #   Poetry -> Libreria que solo permite instalar las dependencias necesarias)
+    #   Poetry -> Libreria que no permite generar conflictos entre dependencias
+    #   pip freeze -> Librerias instaladas
+    #   Black -> Formateador de código
+    #   Instalar dependencias
 """
+
+
 from calculator_class import SimpleCalculator   
 
 if __name__ == "__main__":
@@ -24,15 +39,24 @@ if __name__ == "__main__":
             
             a = float(input("Add first number of the sum:"))
             b = float(input("Add first number of the sum: "))
-            print(f"result:{SimpleCalculator.sum(a,b)}")
+            print(f"result:{SimpleCalculator.suma(a,b)}")
         elif option == 2:
-            pass
+            a = float(input("Add first number of the rest:"))
+            b = float(input("Add first number of the rest: "))
+            print(f"result:{SimpleCalculator.rest(a,b)}")
+            
         elif option == 3:
-            pass
+            a = float(input("Add first number of the multiplication:"))
+            b = float(input("Add first number of the multiplication: "))
+            print(f"result:{SimpleCalculator.multiplication(a,b)}")
         elif option == 4:
-            pass
+            a = float(input("Add first number of the division:"))
+            b = float(input("Add first number of the division: "))
+            print(f"result:{SimpleCalculator.division(a,b)}")
         elif option == 5:
-            pass
+            a = int(input("Add first number of the power:"))
+            b = int(input("Add first number of the power: "))
+            print(f"result:{SimpleCalculator.power(a,b)}")
 
         else: print("Please , choice a valid option")
         option = int(input(MENU))
